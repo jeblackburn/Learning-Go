c51c8a544394238748275f0812412bfa579765da
jeblackburn <me@jonathanblackburn.net>
2011-07-16 21:30:22 -0500

    Remove debugging statement.

--- src/hello/hello_world.go
+++ src/hello/hello_world.go
@@ -8,7 +8,6 @@ import (
 
 func main() {
     greeting := "world!"
-    fmt.Println("len(os.Args) = ", os.Args[0])
     if len(os.Args) > 1 {
         greeting = strings.Join(os.Args[1:], " ")
     }
